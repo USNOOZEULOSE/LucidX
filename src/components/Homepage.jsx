e36abce73fd552684242f3a576ae6a1b43559fd1
sweekiat <sweekiatwong0810@gmail.com>
2023-09-11 11:09:55 +0800

    updating gitignore for sk

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -10,7 +10,7 @@ export const Homepage = () =>{
           </div>
         </div> */}
         <div className="bg-gradient-to-t from-indigo-300 to-indigo-300 w-full h-[500px] flex items-center justify-center">
-  <div className="w-[819px] h-[300px] bg-white bg-opacity-60 rounded-3xl flex align">
+  <div className="w-[819px] h-[300px] bg-white bg-opacity-60 rounded-3xl flex items-center justify-center">
         <Button></Button>
   </div>
 </div>
